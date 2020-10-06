12d8dd55b68bc61ae4cbb582a43ea189680375f3
georgecraciun98 <georgecraciun98@yahoo.com>
2020-10-06 20:50:21 +0300

    Added appearance improvement

--- e-learning/e-learning/asgi.py
+++ e-learning/e-learning/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for educa project.
+ASGI config for e-learning project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e-learning.settings')
 
 application = get_asgi_application()
